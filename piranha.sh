55bdee92dd5eb304d1ae2c5beb2308637bdb1acd
jaxparr0w <39602913+jaxparr0w@users.noreply.github.com>
2018-08-06 15:25:33 -0500

    Update piranha.sh

--- piranha.sh
+++ piranha.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 
 echo " "
-echo "############################" >> log
-echo "## STAGE 1                ##" >> log
-echo "##------------------------##" >> log
-echo "## Running Intense scan...##" >> log
+echo "{-- Running Intense scan... --}" >> log
 echo " "
 # Run Intense scan 
 nmap -v -A -oA intense $1
 
 echo " "
-echo "## Running Vuln scan...##" >> log
+echo "[-- Running Vuln scan... --}" >> log
 echo " "
 # Run Vuln scan 
 nmap -v -sC -sV --script=*vuln* -oA vuln $1
 
-# If 80 or 8080 is open, run nikto and dirb
-if [[ -n $(grep "80/open" intense.gnmap) ]]
+echo " "
+echo "{-- Running Searchsploit... --}" >> log
+echo " "
+# Check sploitsearch
+searchsploit --nmap intense.xml | tee sploitlist
+
+# if 139 open, run enum4linux
+if [[ -n $(grep "139/open" intense.gnmap) ]]
 then
-    echo "## HTTP Found Running Nikto & WFuzz...##" >> log
-    nikto -h $1 | tee niktoscan
-    wfuzz -c -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt --hc 404 http://$1/FUZZ | tee wfuzzscan
+    echo "{-- SMB Found Running Enum4Linux... --}" >> log
+    enum4linux -a $1 | tee enum4linuxscan  
 fi
-if [[ -n $(grep "8080/open" intense.gnmap) ]]
+if [[ -n $(grep "445/open" intense.gnmap) ]]
 then
-    echo "## HTTP Found Running Nikto & WFuzz...##" >> log
-    nikto -h $1 | tee niktoscan
-    wfuzz -c -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt --hc 404 http://$1/FUZZ | tee wfuzzscan
-fi
-if [[ -n $(grep "8080/open" intense.gnmap) ]]
-then
-    echo "## SMB Found Running Enum4Linux...##" >> log
+    echo "{-- SMB Found Running Enum4Linux... --}" >> log
     enum4linux -a $1 | tee enum4linuxscan  
 fi
 
+# If 80 or 8080 is open, run nikto and dirb
+if [[ -n $(grep "80/open" intense.gnmap) ]]
+then
+    echo "{-- HTTP Found Running Nikto & WFuzz... --}" >> log
+    nikto -h $1 | tee niktoscan
+    dirb http://$1 /usr/share/wordlists/dirb/common.txt | tee dirblist
+    wfuzz -c -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt --hc 404 http://$1/FUZZ | tee wfuzzscan
+fi
+if [[ -n $(grep "8080/open" intense.gnmap) ]]
+then
+    echo "{-- HTTP Found Running Nikto & WFuzz... --}" >> log
+    dirb http://$1 /usr/share/wordlists/dirb/common.txt | tee dirblist
+    nikto -h $1 | tee niktoscan
+    wfuzz -c -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt --hc 404 http://$1/FUZZ | tee wfuzzscan
+fi
+
+
 echo " "
-echo "## Running full TCP scan...##" >> log
+echo "{-- Running full TCP scan... --}" >> log
 echo " "
 # Run full TCP scan
 nmap -v -p 1-65355 -oA fullTCP $1
